83730a664b89b12d8320ba10ae1faaeec6fe8bd4
mida111 <65840119+mida111@users.noreply.github.com>
2024-06-02 18:53:31 -0600

    Se agregó la función para seleccionar una fila

--- client/src/components/Table/Table.jsx
+++ client/src/components/Table/Table.jsx
@@ -1,15 +1,30 @@
 import PropsTypes from 'prop-types'
 import { TableHeader } from './TableHeader.jsx'
 import { TableRow } from './TableRow.jsx'
+import { useState } from 'react'
 
-export const Table = ({ columns, data, onEdit, onDelete }) => (
-  <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
-    <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-      <TableHeader columns={columns} />
-      <TableRow data={data} columns={columns} onEdit={onEdit} onDelete={onDelete} />
-    </table>
-  </div>
-);
+export const Table = ({ columns, data, onEdit, onDelete }) => {
+  const [selectedRow, setSelectedRow] = useState(null);
+
+  const handleRowClick = (row) => {
+    setSelectedRow(row);
+  };
+
+  return (
+    <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
+      <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+        <TableHeader columns={columns}/>
+        <TableRow data={data}
+                  columns={columns}
+                  onEdit={onEdit}
+                  onDelete={onDelete}
+                  selectedRow={selectedRow}
+                  onRowClick={handleRowClick}
+        />
+      </table>
+    </div>
+  )
+}
 
 Table.propTypes = {
   columns: PropsTypes.array.isRequired,
--- client/src/components/Table/TableRow.jsx
+++ client/src/components/Table/TableRow.jsx
@@ -1,35 +1,44 @@
 import PropsTypes from 'prop-types'
 
-export const TableRow = ({ data, columns, onEdit, onDelete }) => (
+export const TableRow = ({ data, columns, onEdit, onDelete, selectedRow, onRowClick }) => (
   <tbody>
-  {data.map((row, rowIndex) => (
-    <tr
-      className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600"
-      key={rowIndex}
-    >
-      {columns.map((column) => (
-        <td className="px-6 py-4" key={column.accessor}>
-          {row[column.accessor]}
+    {data.map((row) => (
+      <tr
+        key={row.id}
+        onClick={() => onRowClick(row)}
+        className={`bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600 ${
+          selectedRow && selectedRow.id === row.id ? 'bg-gray-200 dark:bg-gray-900' : ''
+        }`}
+      >
+        {columns.map((column) => (
+          <td className="px-6 py-4" key={column.accessor}>
+            {row[column.accessor]}
+          </td>
+        ))}
+        <td className="flex items-center px-6 py-4">
+          <a
+            href="#"
+            className="font-medium text-blue-600 dark:text-blue-500 hover:underline"
+            onClick={(e) => {
+              e.stopPropagation(); // Evita que el click en Editar seleccione la fila
+              onEdit(row.id);
+            }}
+          >
+            Editar
+          </a>
+          <a
+            href="#"
+            className="font-medium text-red-600 dark:text-red-500 hover:underline ms-3"
+            onClick={(e) => {
+              e.stopPropagation(); // Evita que el click en Eliminar seleccione la fila
+              onDelete(row.id);
+            }}
+          >
+            Eliminar
+          </a>
         </td>
-      ))}
-      <td className="flex items-center px-6 py-4">
-        <a
-          href="#"
-          className="font-medium text-blue-600 dark:text-blue-500 hover:underline"
-          onClick={() => onEdit(row.id)}
-        >
-          Editar
-        </a>
-        <a
-          href="#"
-          className="font-medium text-red-600 dark:text-red-500 hover:underline ms-3"
-          onClick={() => onDelete(row.id)}
-        >
-          Eliminar
-        </a>
-      </td>
-    </tr>
-  ))}
+      </tr>
+    ))}
   </tbody>
 )
 
@@ -37,5 +46,7 @@ TableRow.propTypes = {
   data: PropsTypes.array.isRequired,
   columns: PropsTypes.array.isRequired,
   onEdit: PropsTypes.func.isRequired,
-  onDelete: PropsTypes.func.isRequired
+  onDelete: PropsTypes.func.isRequired,
+  selectedRow: PropsTypes.object,
+  onRowClick: PropsTypes.func.isRequired,
 }
